01b7dbcf104cb194e196b2c2c6998aa283b11c8f
Chettapong Pinsuwan <chettapong.work@gmail.com>
2018-01-18 20:41:20 +0700

    add param

--- run-miner.sh
+++ run-miner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/miner/ccminer/ccminer --algo=$ALGO --url="$MINING_POOL" --userpass="$USER_NAME:$PASSWORD"
+/home/miner/ccminer/ccminer --algo=$ALGO --url="$MINING_POOL" --userpass="$USER_NAME:$PASSWORD" --api-bind=port="$API_IP:$API_PORT"
