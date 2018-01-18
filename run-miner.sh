0ed50a29605e5f33753c6170bb9020605435e48a
Chettapong Pinsuwan <chettapong.work@gmail.com>
2018-01-19 03:52:45 +0700

    add ip, port api

--- run-miner.sh
+++ run-miner.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-/home/miner/ccminer/ccminer --algo=$ALGO --url="$MINING_POOL" --userpass="$USER_NAME:$PASSWORD" --api-bind=port="$API_IP:$API_PORT"
+getApiBind() {
+  if [ ! -z "$API_IP" ]; then
+    IP=$API_IP
+  else
+    IP="127.0.0.1"
+  fi
+
+  if [ ! -z "$API_PORT" ]; then
+    PORT=$API_PORT
+  else
+    PORT="4068"
+  fi
+
+  echo "$IP:$PORT"
+}
+
+/home/miner/ccminer/ccminer --algo=$ALGO --url="$MINING_POOL" --userpass="$USER_NAME:$PASSWORD" --api-bind="$(getApiBind)"
